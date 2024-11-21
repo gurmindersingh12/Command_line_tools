941f3c48f02a354392ee0235bb36ac3d15981fec
Gurminder Singh <123406976+gurmindersingh12@users.noreply.github.com>
2024-11-20 23:16:36 -0600

    Update linux_scripts.sh

--- linux_scripts.sh
+++ linux_scripts.sh
@@ -13,4 +13,18 @@ $ find [location] [criteria] [actions]
 ## if no criteria is given, all the files are displayed; and if no actions are given,
 ## only a listing of the names is given.
 
+## There are many logical expressions which can be used for criteria.
+
+## For example, we can use
+$ find
+## this will give everything, all directories and its descendants. no need to run that, may not be very helpful;
+## however using with criteria or action command will be a very powerful tool.
+
+$ find /root_folder/home/username/directory_name -name "*.fasta"
+
+## location = /root_folder/home/username/directory_name
+## criteria = -name "*.fasta"
+
+## the `*` is a wildcard, meaning it will match any string of characters.
+## the above code will search through `directory_name` and all its sub-direactories for files with names ending in `.fasta'
 
