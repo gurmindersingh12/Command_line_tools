fac058f1b9de76d34deb7337e7ffc18c88c24437
Gurminder Singh <123406976+gurmindersingh12@users.noreply.github.com>
2024-11-21 02:03:43 +0000

    Practice codes

---
+++ linux_scripts.sh
@@ -0,0 +1,16 @@
+#######################################################################
+#################  Finding Files: find and locate  ####################
+#######################################################################
+
+
+## General form of a `find` command:
+
+$ find [location] [criteria] [actions]
+
+### there are three classes of the arguments, each of which can be omitted.
+
+## for example, if no location is given, then the current directory (.) is selected or sssumed.
+## if no criteria is given, all the files are displayed; and if no actions are given,
+## only a listing of the names is given.
+
+
